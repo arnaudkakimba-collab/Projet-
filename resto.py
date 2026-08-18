8b30528c0572f0d8d7fd14f358864cfbf3f2b317
Arnaud <arnaudkakimba@gmail.com>
2026-08-18 23:27:42 +0300

    new

--- resto.py
+++ resto.py
@@ -139,8 +139,8 @@ def main(page:ft.Page):
       
     page.add(colone)
 
-ft.run(
-    main,
+ft.app(
+    target=main,
     view=ft.AppView.WEB_BROWSER,
     host="0.0.0.0",
     port=int(os.environ.get("PORT", 10000))
